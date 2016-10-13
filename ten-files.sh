202d7b26d2a1dd9e88a99672cbe166987d9821a5
Craig Overton <craig_overton@ymail.com>
2016-10-12 19:18:12 -0500

    Adding test question 5

---
+++ ten-files.sh
@@ -0,0 +1,15 @@
+#!\bin\bash
+
+cd ~/test01/question-five
+echo -e 'The crags are the best part\!' > crumpets01.txt 
+echo -e 'The crags are the best part\!' > crumpets02.txt
+echo -e 'The crags are the best part\!' > crumpets03.txt
+echo -e 'The crags are the best part\!' > crumpets04.txt
+echo -e 'The crags are the best part\!' > crumpets05.txt
+echo -e 'My present working directory is: ~/test01/question-five' > crumpets06.txt
+echo -e 'My present working directory is: ~/test01/question-five' > crumpets07.txt
+echo -e 'My present working directory is: ~/test01/question-five' > crumpets08.txt
+echo -e 'My present working directory is: ~/test01/question-five' > crumpets09.txt
+echo -e 'My present working directory is: ~/test01/question-five' > crumpets10.txt
+ 
+    
